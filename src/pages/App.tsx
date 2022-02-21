3c346753ca801a0aa1a598d9b7c5177393c8be43
xf <sadfx41@gmail.com>
2022-02-21 11:23:42 +0800

    fix: alert notice zIndex

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -64,7 +64,7 @@ export default function App() {
     <Suspense fallback={null}>
       <ModalProvider>
         <div id="app">
-          <Box sx={{ position: 'fixed', width: '100%' }}>
+          <Box sx={{ position: 'fixed', width: '100%', zIndex: 999 }}>
             <Alert
               message={
                 <>
